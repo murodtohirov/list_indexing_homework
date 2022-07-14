e29daa19456fe40ea9cc90e077086275103379fa
Пользователь <murodjontohirov2701@gmail.com>
2022-07-14 12:56:42 +0500

    list09

--- bin/list_index09.dart
+++ bin/list_index09.dart
@@ -4,6 +4,28 @@
     Args:
         list1 (list): parameter
     Returns:
+    
+    
         bool: return answer
 */
-void main() {}
+
+bool func(list1) {
+  int q = 0;
+  bool w = true;
+  List y = [];
+  while (q <= list1.length - 1) {
+    if (list1[q] == list1[list1.length - 1]) {
+      y.add(true);
+    }
+    q++;
+  }
+  if (y.length == list1.length) {
+    w = true;
+  } else
+    w = false;
+  return w;
+}
+
+void main() {
+  print(func(['h0', 'le', 'oo']));
+}
